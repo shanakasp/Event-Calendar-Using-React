4c4aec71e5d4dc9acc227aa4a6aae29a37e96f9c
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-12 13:34:04 +0530

    Add files via upload

---
+++ Calender/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Calender } from "./Calender";
+
+function App() {
+  return (
+    <div className="App">
+       <Calender />
+    </div>
+  );
+}
+
+export default App;
